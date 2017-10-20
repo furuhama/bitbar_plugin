09d47fed2af0183054cb76cc78ef01c42093c659
furuhama <furuhama@users.noreply.github.com>
2017-10-21 00:33:38 +0900

    first commit(simple plugin)

---
+++ plugin.sh
@@ -0,0 +1,18 @@
+#!/bin/zsh
+
+# menubar icon
+echo ':sushi:'
+echo '---'
+###################
+# 以下がメニューに
+###################
+
+# display date
+date
+
+# open google
+echo 'google | href=https://google.co.jp/'
+
+# open devdocs
+echo 'devdocs | href=http://devdocs.io/'
+
