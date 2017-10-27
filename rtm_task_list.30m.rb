f056d44e69f0c13c3db0698178aacb659ed1ee5a
furuhama <furuhama@users.noreply.github.com>
2017-10-27 23:26:33 +0900

    view Remember the Milk tasks in menubar

--- rtm_task_list.30m.rb
+++ rtm_task_list.30m.rb
@@ -1,4 +1,61 @@
-#!/bin/zsh
+#!/bin/sh
 echo ':cow:'
 echo ---
-echo "|image='~/Downloads/s_rtm.png'"
+
+shopt -s expand_aliases
+source ~/Documents/bitbar_plugin/.env
+
+exec ruby -S -x "$0" "$@"
+
+#!/usr/bin/env ruby
+
+# this plugin uses Milkman gem
+# https://github.com/kevintuhumury/milkman
+require 'milkman'
+
+# load ENV variables
+api_key = ENV['RTM_API_KEY']
+shared_secret = ENV['RTM_SHARED_SECRET']
+auth_token = ENV['RTM_AUTH_TOKEN']
+
+# set Milkman instance
+client = Milkman::Client.new(api_key: api_key,
+                             shared_secret: shared_secret,
+                             auth_token: auth_token)
+
+##################
+# private tasks
+##################
+puts '******  private tasks  ******'
+
+# set list_id
+private_list_id = ENV['PRIVATE_LIST_ID']
+
+# send api & get json type response
+private_json = client.get('rtm.tasks.getList', { list_id: private_list_id, filter: "status: incomplete" })
+
+# parse response (returns "list of hash")
+private_tasks = private_json['rsp']['tasks']['list'][0]['taskseries']
+
+private_tasks.each do |task|
+  puts "#{task['name']}  [#{task['tags']['tag']}]| color=blue"
+end
+
+##################
+# work tasks
+##################
+puts '******  work tasks  ******'
+
+# set list_id
+work_list_id = ENV['WORK_LIST_ID']
+
+# send api & get json type response
+work_json = client.get('rtm.tasks.getList', { list_id: work_list_id, filter: "status: incomplete" })
+
+# parse response (returns "list of hash")
+work_tasks = work_json['rsp']['tasks']['list'][0]['taskseries']
+
+work_tasks.each do |task|
+  puts "#{task['name']}  [#{task['tags']['tag']}]| color=green"
+end
+
